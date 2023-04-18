a5caa54b7d91437eb5073c7b5acb0e77f7f0ac97
n1colashka <deckards2505@gmail.com>
2023-04-18 19:39:02 +0600

    fixes

--- src/js/components/scroll.js
+++ src/js/components/scroll.js
@@ -1,15 +1,7 @@
 import SmoothScroll from 'smooth-scroll';
-import PerfectScrollbar from 'perfect-scrollbar';
 
 // Плавный скролл
 const scroll = new SmoothScroll('a[href*="#"]', {
   speedAsDuration: true,
   duration: 500,
 });
-
-// Кастомный скроллбар
-const scrollBarContainers = document.querySelectorAll('.perfect-scrollbar');
-
-scrollBarContainers?.forEach(el => {
-  const ps = new PerfectScrollbar(el);
-})
